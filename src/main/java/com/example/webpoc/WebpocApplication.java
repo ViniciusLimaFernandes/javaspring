91de79373ea95b3948ca4ff9ca537a75514a9fb1
viniciuslimafernandes <viniciuslima.4k@gmail.com>
2020-02-21 09:46:10 -0300

    First commit

---
+++ src/main/java/com/example/webpoc/WebpocApplication.java
@@ -0,0 +1,13 @@
+package com.example.webpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webpoc/WebpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
